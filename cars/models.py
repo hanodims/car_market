eef300167434c6e34a9d441b72be5d2c05689b6e
hanodims <hend--mohammed@outlook.com>
2020-09-05 20:03:40 +0300

    still working on CSS

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,8 @@ class Car(models.Model):
 	make = models.CharField(max_length=120)
 	model = models.CharField(max_length=120)
 	year = models.IntegerField()
+	img = models.ImageField(blank=True)
+	
 
 	def __str__(self):
 		return "{} {} - {}".format(self.make, self.model, self.year)
